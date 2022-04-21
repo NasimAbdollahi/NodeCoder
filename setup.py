1f46ba28dcf832a35ec6d76d440ffa71a530dd19
NasimAbdollahi <nsm.abdolahi@gmail.com>
2022-04-21 10:21:04 -0400

    minor update.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/NasimAbdollahi/Nodecoder/'
 EMAIL = 'nsm.abdolahi@gmail.com'
 AUTHOR = 'Nasim Abdollahi'
 REQUIRES_PYTHON = '>=3.8.5'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
